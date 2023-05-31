d2d4bf562b6ed9e7424f5c659c2caab296adff34
Maciej Mroz <mrozman24@gmail.com>
2023-05-31 11:44:39 +0200

    Fix footer on the bottom of the page

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 import { useTranslation } from 'react-i18next';
 import Header from './components/Header';
-import BoxButtonsSection from './components/BoxButtonSection';
-import WelcomeSection from './components/WelcomeSection';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 function App() {
   const { t } = useTranslation();
   return (
-    <div>
+    <div className="app">
       <Header />
       <Outlet />
       <Footer />
