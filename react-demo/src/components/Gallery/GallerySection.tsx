d917e657a36f0e527e69aa54a6960d644c7fe07f
Maciej Mroz <mrozman24@gmail.com>
2023-06-21 20:02:59 +0200

    Add hover for GallerySection Links and fix styling

--- react-demo/src/components/Gallery/GallerySection.tsx
+++ react-demo/src/components/Gallery/GallerySection.tsx
@@ -3,15 +3,25 @@ import PhotoModalData from '../../dataModels/PhotoModalData';
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useLoaderData } from 'react-router-dom';
+import { useState } from 'react';
 const GallerySection = () => {
   const photos: PhotoModalData[] = useLoaderData() as PhotoModalData[];
+  const [hoverIndex, setHoverIndex] = useState<number>();
+
+  const mouseEnterHandler = (index: number): void => {
+    setHoverIndex(index);
+  };
+
+  const mouseLeaveHandler = (): void => {
+    setHoverIndex(undefined);
+  };
 
   return (
     <div className={styles.gallery}>
       <div className="left"></div>
       <div className={styles.gallerySection}>
         <Outlet />
-        {photos.map((photo: PhotoModalData) => {
+        {photos.map((photo: PhotoModalData, index: number) => {
           return (
             <Link
               style={{
@@ -22,9 +32,15 @@ const GallerySection = () => {
                 backgroundImage: `url(${photo.photoUrl})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
-                border: '2px solid black',
+                opacity: index === hoverIndex ? '70%' : '100%',
+                border:
+                  index === hoverIndex
+                    ? '2px solid #9fd3c7'
+                    : '2px solid black',
                 margin: '10px 10px',
               }}
+              onMouseEnter={() => mouseEnterHandler(index)}
+              onMouseLeave={mouseLeaveHandler}
               to={photo.photoId.toString()}
               key={photo.photoId}
             ></Link>
